5621b1b937c9d68c3017948e1a349ed83e2b79c5
Josenetto97 <66394810+Josenetto97@users.noreply.github.com>
2023-07-22 11:30:57 -0300

    implementação do carro

--- src/routes/create-car.js
+++ src/routes/create-car.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const database = require('../database')
 
 const route = express()
 
-route.post('/car', () =>{
+route.post('/car', (req, res) =>{
+    //obtem os dados do body da requisição
+    const car = req.body
+    
+    //adiciona o carro no array do database
+    database.push(car)
 
+    //retorna a resposta do status 201 e o carro cadastrado
+    return res.status(201).json(car)
 })
 
 module.exports = route
